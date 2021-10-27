a848729afedb512eea57c618b6c5fe1cf3b1e18d
Juan Fernando <juanfed12345@gmail.com>
2021-10-27 18:18:09 -0500

    Servidor funcionando perfectamente

--- servidor.js
+++ servidor.js
@@ -1,31 +1,62 @@
 import Express from "express";
-import { MongoClient } from 'mongodb'; // para conectarnos a la base de datos y trabajar con mongoclient
+import { MongoClient, ObjectId } from 'mongodb'; // para conectarnos a la base de datos y trabajar con mongoclient
+import Cors from 'cors';
 
 const stringConexion =  //conexion con la basse de datos
   'mongodb+srv://juanfed:0707MT60FZxSp35@proyectomisiontic.vsvmd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const client = new MongoClient(stringConexion, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+  const client = new MongoClient(stringConexion, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
 let conexion; // variable global
 
 
 // Creo la aplicacion del backend
 const app = Express();
+
 app.use(Express.json()) // para que la solicitud se pase por aca y se desgloce
+app.use(Cors());
+
 
 app.get('/producto', (req, res) => { // ruta de tipo get
   console.log("estoy haciendo  una peticion get");
-  conexion.collection("productos").find({}).limit(50).toArray((err, result) =>{
-    if(err){
+  conexion.collection("productos").find().limit(50).toArray((err, result) => {
+    if (err) {
       res.status(500).send("Error al consultar los productos");
     }
-    else{
+    else {
       res.json(result);
     }
   });
+});
+
+// solicitud de tipo patch
+app.patch('/producto/edit', (req, res) => { 
+  const edicion = req.body;
+  console.log("servio de tipo patch realizada");
+  console.log(edicion);
+  const filtroProducto = { _id: new ObjectId(edicion.id) };
+  delete edicion.id;
+  const operacion = {
+    $set: edicion,
+  };
+  conexion
+    .collection('producto')
+    .findOneAndUpdate(
+      filtroProducto,
+      operacion,
+      { upsert: true, returnOriginal: true },
+      (err, result) => {
+        if (err) {
+          console.error('error actualizando el producto: ', err);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(200);
+        }
+      }
+    );
 });
 
 // solicitud de tipo post
@@ -42,7 +73,7 @@ app.post('/producto/nuevo', (req, res) => {
       Object.keys(datosProductos).includes('estado')
     ) {
       // implementacion del codigo para la creacion del producto
-      conexion.collection('productos').insertOne(datosProductos, (err, result) => {
+      conexion.collection('producto').insertOne(datosProductos, (err, result) => {
         if (err) {
           res.sendStatus(500);
         } else {
@@ -60,6 +91,19 @@ app.post('/producto/nuevo', (req, res) => {
   }
 });
 
+//solicitud de tipo delete
+app.delete('/producto/delete', (req, res) => {
+  const filtroProducto = { _id: new ObjectId(req.body.id) };
+  conexion.collection('producto').deleteOne(filtroProducto, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
 
 
 const main = () => { // se conecta y me retorna en la base de datos
@@ -67,13 +111,14 @@ const main = () => { // se conecta y me retorna en la base de datos
   client.connect((err, db) => {
     if (err) {
       console.error("Error a la hora de coenctar a la base de datos");
+      return 'error';
     }
     conexion = db.db('celulares');
     console.log("conexion exitosa")
     return app.listen(5000, () => { // abro el puerto en donde se ejecutará
       console.log("ejecutando el servidor");
     });
-  })
+  });
 
 };
 
