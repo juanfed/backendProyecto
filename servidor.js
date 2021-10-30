b79a9888c32e021623909c7e2799047c4d0e9816
Juan Fernando <juanfed12345@gmail.com>
2021-10-30 17:06:36 -0500

    funcioamiento metodo get en ventas

--- servidor.js
+++ servidor.js
@@ -19,7 +19,6 @@ const app = Express();
 app.use(Express.json()) // para que la solicitud se pase por aca y se desgloce
 app.use(Cors());
 
-
 app.get('/producto', (req, res) => { // ruta de tipo get
   console.log("productos actualizados");
   conexion.collection("productos").find({}).limit(50).toArray((err, result) => {
@@ -100,6 +99,52 @@ app.delete('/producto/delete', (req, res) => {
     }
   });
 });
+
+
+// -------------solicutudes para ventas-----------
+app.post('/ventas/nuevo', (req, res) => {
+  console.log(req);
+  const datosProductos = req.body;
+  console.log('llaves: ', Object.keys(datosProductos));
+  try {
+    if (
+      Object.keys(datosProductos).includes('valorTotal') &&
+      Object.keys(datosProductos).includes('cantidad') &&
+      Object.keys(datosProductos).includes('valorUnidad') &&
+      Object.keys(datosProductos).includes('fechaVenta') &&
+      Object.keys(datosProductos).includes('idCliente') &&
+      Object.keys(datosProductos).includes('nombreCliente') &&
+      Object.keys(datosProductos).includes('estado')
+    ) {
+      // implementacion del codigo para la creacion del producto
+      conexion.collection('ventas').insertOne(datosProductos, (err, result) => {
+        if (err) {
+          res.sendStatus(500);
+        } else {
+          console.log(result);
+          res.sendStatus(200);
+        }
+      });
+    } else {
+      res.sendStatus(500);
+    }
+  } catch {
+    res.sendStatus(500);
+  }
+});
+
+// solictud de tipo get
+app.get('/ventas', (req, res) => { // ruta de tipo get
+  console.log("ventas actualizadas");
+  conexion.collection('ventas').find({}).limit(50).toArray((err, result) => {
+    if (err) {
+      res.status(500).send("Error al consultar las ventas");
+    }
+    else {
+      res.json(result);
+    }
+  });
+}); 
 
 
 
